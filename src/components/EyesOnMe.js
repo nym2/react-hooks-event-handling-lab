0bb84b1a93b8e1a3c46e375db84507f016807313
NymphaPamba <praisepamba@gmail.com>
2024-11-11 12:04:59 +0300

    initial commits

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+    function attention(event) {
+        if(event.type === 'focus'){
+            console.log('Good!')
+        }
+        else if(event.type === 'blur'){
+            console.log('Hey! Eyes on me!')
+        }
+    }
+    return (
+        <button onFocus={attention} onBlur={attention}>'Eyes on me'</button>
+    )
+}
+
+
+export default EyesOnMe;
